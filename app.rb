c7b00f55a5e9bb6ae1dcf4fbb7be73cec7a8b720
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-06-13 13:27:07 -0400

    WIP: Build out random word list method
    - Add logic to exclude all but desired part of speech from api call
    to increase result quality.

--- app.rb
+++ app.rb
@@ -19,19 +19,56 @@ Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each do |file|
   also_reload file
 end
 
-def wordnik
+def get_random_words(word_type) # dictionary_def, frequency)
+
   Wordnik.configure do |config|
       config.api_key = "#{ENV["API_KEY"]}"
   end
 
-  definition = Wordnik.words.get_random_words(:limit => 5)
+  types_to_exclude = parts_of_speech
+  types_to_exclude.delete(word_type)
+  types_to_exclude
 
+  # word_list = Wordnik.words.get_random_words(
+  #
+  # )
+
+end
+
+def parts_of_speech
+  pos = ["noun",
+        "adjective",
+        "verb",
+        "adverb",
+        "interjection",
+        "pronoun",
+        "preposition",
+        "abbreviation",
+        "affix",
+        "article",
+        "auxilliary-verb",
+        "conjunction",
+        "definite-article",
+        "family-name",
+        "given-name",
+        "idiom",
+        "imperative",
+        "noun-plural",
+        "noun-possessive",
+        "past-participle",
+        "phrasal-prefix",
+        "proper-noun",
+        "proper-noun-plural",
+        "proper-noun-possessive",
+        "suffic",
+        "verb-intransitive",
+        "verb-transitive"]
 end
 
 
 get '/' do
 
-  definition = wordnik
+  definition = get_random_words("noun")
 
   erb :index, locals: { definition: definition }
 end
