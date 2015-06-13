c807efc6848d2dc6ae4b011237ff5f75cd1bbce8
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-06-13 13:05:55 -0400

    WIP: Building out methods to seed db from various data sources.

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/reloader'
+require 'wordnik'
+require 'dotenv'
+
+Dotenv.load
 
 configure :development, :test do
   require 'pry'
@@ -15,7 +19,19 @@ Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each do |file|
   also_reload file
 end
 
+def wordnik
+  Wordnik.configure do |config|
+      config.api_key = "#{ENV["API_KEY"]}"
+  end
+
+  definition = Wordnik.words.get_random_words(:limit => 5)
+
+end
+
+
 get '/' do
-  @title = "Hello World"
-  erb :index
+
+  definition = wordnik
+
+  erb :index, locals: { definition: definition }
 end
---
+++ app/models/word.rb
@@ -0,0 +1,10 @@
+class Word
+
+attr_accessor :word, :part_of_speech
+
+  def initialize()
+    @word = word
+    @part_of_speech = part_of_speech
+  end
+
+end
